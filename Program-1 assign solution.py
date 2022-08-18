bc539feb044c7c6b0687c1f7d3e0f96f7236c017
Niyati Sharma <86042473+Niyatisharma2@users.noreply.github.com>
2022-08-18 12:56:30 +0530

    Add files via upload

---
+++ Program-1 assign solution.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 17 23:20:13 2022
+
+@author: NIYATI SHARMA
+"""
+
+#PYTHON PROGRAM ASSIGNMNT-1
+#Q.1> WRITE A PROGRAM TO PRINT HELLO PYTHON .
+print( "Hello Python")
+
+#Q2.> Write a Python program to do arithmetical operations addition and division.?
+a=2
+b=6
+c=a+b
+e=a-b
+f=a*b
+d=a/b
+print(c,d,e,f)
+
+#Q3>Write a Python program to find the area of a triangle?
+b=int(input("25 the cale of base a="))
+h=int(input("25 the value of height value h="))
+Area = 1/2(b*h)
+print(Area)
+print("hello")
+#Q4> Write a Python program to swap two variables?
+a=b
+temp=b
+a=temp
+print(a)
+#Q4>Write a Python program to generate a random number?
+print(random(5,20))
+
+
