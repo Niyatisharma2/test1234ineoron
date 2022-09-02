0dd8740800dbad056c926028406cabaa6e95bb47
Niyati Sharma <86042473+Niyatisharma2@users.noreply.github.com>
2022-09-02 22:21:29 +0530

    Add files via upload

---
+++ ProgrammeAssignment8.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  2 22:13:49 2022
+
+@author: NIYATI SHARMA
+"""
+
+#Q1.>Write a Python Program to Add Two Matrices?###
+# Program to add two matrices using nested loop
+
+X = [[12,7,3],
+    [4 ,5,6],
+    [7 ,8,9]]
+
+Y = [[5,8,1],
+    [6,7,3],
+    [4,5,9]]
+
+result = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[i][j] = X[i][j] + Y[i][j]
+
+for r in result:
+   print(r)
+
+#Q2.>Write a Python Program to Multiply Two Matrices?###
+# Program to multiply two matrices using nested loops
+
+# 3x3 matrix
+X = [[5,7,3],
+    [4 ,5,6],
+    [7 ,0,9]]
+# 3x4 matrix
+Y = [[5,8,1,2],
+    [6,5,3,0],
+    [4,10,9,1]]
+# result is 3x4
+result = [[0,0,0,0],
+         [0,0,0,0],
+         [0,0,0,0]]
+
+# iterate through rows of X
+for i in range(len(X)):
+   # iterate through columns of Y
+   for j in range(len(Y[0])):
+       # iterate through rows of Y
+       for k in range(len(Y)):
+           result[i][j] += X[i][k] * Y[k][j]
+
+for r in result:
+   print(r)
+
+#Q3.>Write a Python Program to Transpose a Matrix?$$$$
+
+X = [[12,7],
+    [4 ,5],
+    [3 ,8]]
+
+result = [[0,0,0],
+         [0,0,0]]
+
+# iterate through rows
+for i in range(len(X)):
+   # iterate through columns
+   for j in range(len(X[0])):
+       result[j][i] = X[i][j]
+
+for r in result:
+   print(r)
+
+#Q4.>Write a Python Program to Sort Words in Alphabetic Order?####
+
+my_str = "Hello this Is an Example With cased letters"
+
+# To take input from the user
+#my_str = input("Enter a string: ")
+
+# breakdown the string into a list of words
+words = [word.lower() for word in my_str.split()]
+
+# sort the list
+words.sort()
+
+# display the sorted words
+
+print("The sorted words are:")
+for word in words:
+   print(word)
+#Q5.>Write a Python Program to Remove Punctuation From a String?###
+ #define punctuation
+punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+my_str = "Hello!!!, he said ---and went."
+
+# To take input from the user
+# my_str = input("Enter a string: ")
+
+# remove punctuation from the string
+no_punct = ""
+for char in my_str:
+   if char not in punctuations:
+       no_punct = no_punct + char
+
+# display the unpunctuated string
+print(no_punct)
